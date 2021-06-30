d0b62e0bcd20ef4686abad2c4015c69ecc1a5076
halbakri <84340692+halbakri@users.noreply.github.com>
2021-06-30 14:52:18 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
-repositories = ['bat', 'bowtie2']
+#List repositories
+repositories = ['bat', 'bowtie2','fastani','phylip-suite','blast','bwa','picard','gatk','hisat2','abyss','trimmomatic','graphviz','hyperfine','dust','tiger','gent','asciinema','trimmomatic','methylpy','octave']
 
 #sort repositories
+epositories.sort()
 
 for repository in repositories:
-  #print the line
-  #| [bat](http://github.com/pscedu/singularity-bat) |![Status](https://github.com/pscedu/singularity-bat/actions/workflows/main.yml/badge.svg)![Issue](https://img.shields.io/github/issues/pscedu/singularity-bat)![forks](https://img.shields.io/github/forks/pscedu/singularity-bat)![Stars](https://img.shields.io/github/stars/pscedu/singularity-bat)![License](https://img.shields.io/github/license/pscedu/singularity-bat) |
+    new_string = string.replace(  "#| [repository](http://github.com/pscedu/singularty-"+str(repository),") |![Status](https://github.com/pscedu/singularity-"+str(repository),"/actions/workflows/main.yml/badge.svg)![Issue](https://img.shields.io/github/issues/pscedu/singularity-"+str(repository),")![forks](https://img.shields.io/github/forks/pscedu/singularity-"+str(repository),")![Stars](https://img.shields.io/github/stars/pscedu/singularity-"+str(repository),")![License](https://img.shields.io/github/license/pscedu/singularity-"+str(repository),") |
+)"
+    new_strings.append(new_string)
+print (new_strings)
