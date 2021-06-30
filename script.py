e170af6ccef138bc3265562dd5bd4b2df382acbe
icaoberg <icaoberg@andrew.cmu.edu>
2021-06-30 11:22:16 -0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,7 @@
+repositories = ['bat', 'bowtie2']
+
+#sort repositories
+
+for repository in repositories:
+  #print the line
+  #| [bat](http://github.com/pscedu/singularity-bat) |![Status](https://github.com/pscedu/singularity-bat/actions/workflows/main.yml/badge.svg)![Issue](https://img.shields.io/github/issues/pscedu/singularity-bat)![forks](https://img.shields.io/github/forks/pscedu/singularity-bat)![Stars](https://img.shields.io/github/stars/pscedu/singularity-bat)![License](https://img.shields.io/github/license/pscedu/singularity-bat) |
