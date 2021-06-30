ff0f2384b9b3c167808fb77669defb761c4d29eb
Ivan Cao-Berg <icaoberg@psc.edu>
2021-06-30 17:10:22 -0400

    Updated script and README file

--- script.py
+++ script.py
@@ -1,8 +1,20 @@
-repositories = ['bat', 'bowtie2']
-
 #sort repositories
 file = open("README.md", "w")
 
+# STEM
+repositories = ['bat', 'bowtie2']
+file.write('#List of Singularity containers\n')
+file.write('## STEM\n')
+file.write( '| Name | Information |\n' )
+file.write( '| --- | --- |\n' )
+
+for repository in repositories:
+  str = '| [' + repository + '](http://github.com/pscedu/singularity-' + repository + ') | ![Status](https://github.com/pscedu/singularity-' + repository + '/actions/workflows/main.yml/badge.svg)![Issue](https://img.shields.io/github/issues/pscedu/singularity-' + repository + ')![forks](https://img.shields.io/github/forks/pscedu/singularity-' + repository + ')![Stars](https://img.shields.io/github/stars/pscedu/singularity-' + repository + ')![License](https://img.shields.io/github/license/pscedu/singularity-' + repository + ') |\n'
+  file.write( str )
+
+# Utilities
+repositories = ['bat']
+file.write('\n## Utiliiies\n')
 file.write( '| Name | Information |\n' )
 file.write( '| --- | --- |\n' )
 
