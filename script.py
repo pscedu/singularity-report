d773c5d82603f0c7bd529fa64099b8d96944ccf1
Ivan Cao-Berg <icaoberg@psc.edu>
2021-06-30 17:03:09 -0400

    Updated script

--- script.py
+++ script.py
@@ -1,7 +1,13 @@
 repositories = ['bat', 'bowtie2']
 
 #sort repositories
+file = open("README.md", "w")
+
+file.write( '| Name | Information |\n' )
+file.write( '| --- | --- |\n' )
 
 for repository in repositories:
-  #print the line
-  #| [bat](http://github.com/pscedu/singularity-bat) |![Status](https://github.com/pscedu/singularity-bat/actions/workflows/main.yml/badge.svg)![Issue](https://img.shields.io/github/issues/pscedu/singularity-bat)![forks](https://img.shields.io/github/forks/pscedu/singularity-bat)![Stars](https://img.shields.io/github/stars/pscedu/singularity-bat)![License](https://img.shields.io/github/license/pscedu/singularity-bat) |
+  str = '| [' + repository + '](http://github.com/pscedu/singularity-' + repository + ') | ![Status](https://github.com/pscedu/singularity-' + repository + '/actions/workflows/main.yml/badge.svg)![Issue](https://img.shields.io/github/issues/pscedu/singularity-' + repository + ')![forks](https://img.shields.io/github/forks/pscedu/singularity-' + repository + ')![Stars](https://img.shields.io/github/stars/pscedu/singularity-' + repository + ')![License](https://img.shields.io/github/license/pscedu/singularity-' + repository + ') |\n'
+  file.write( str )
+
+file.close()
