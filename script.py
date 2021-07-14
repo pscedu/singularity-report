fc76d3910ed97c54ffd3231d00adac2de2c3db64
halbakri <84340692+halbakri@users.noreply.github.com>
2021-07-14 10:29:28 -0400

    Update script.py

--- script.py
+++ script.py
@@ -14,7 +14,7 @@ for repository in repositories:
   file.write( str )
 
 # Utilities
-repositories = ['bat','graphviz','hyperfine','dust','asciinema','rclone']
+repositories = ['bat','graphviz','hyperfine','dust','asciinema','rclone','ffmpeg']
 repositories.sort()
 file.write('\n## Utilities\n')
 file.write( '| Name | Information |\n' )
