c91cd4ab6e1c5290fc539fa740b974a71d557b11
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2021-08-04 23:04:22 -0400

    Updated repo

--- script.py
+++ script.py
@@ -2,7 +2,7 @@
 file = open("README.md", "w")
 
 # STEM
-repositories = ['bowtie2','prodigal','samtools','fastani','phylip-suite','blast','viennarna','cutadapt','bismark','star','prodigal','bwa','picard','hisat2','abyss','octave','trimmomatic','tiger','gent','methylpy','fasttree']
+repositories = ['bowtie2','prodigal','spades','busco','samtools','fastani','phylip-suite','blast','viennarna','cutadapt','bismark','star','prodigal','bwa','picard','hisat2','abyss','octave','trimmomatic','tiger','gent','methylpy','fasttree']
 repositories.sort()
 file.write('# List of Singularity containers\n')
 file.write('## STEM\n')
