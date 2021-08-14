7309e005e8855dd178204dc8ac01d88ff35179b8
icaoberg <icaoberg@andrew.cmu.edu>
2021-08-14 17:50:07 -0400

    Update script.py

--- script.py
+++ script.py
@@ -21,7 +21,7 @@ file.write( '| Name | Information |\n' )
 file.write( '| --- | --- |\n' )
 
 for repository in repositories:
-  str = '| [' + repository + '](http://github.com/pscedu/singularity-' + repository + ') | ![Status](https://github.com/pscedu/singularity-' + repository + '/actions/workflows/main.yml/badge.svg)!![Status](https://github.com/pscedu/singularity-' + repository + '/actions/workflows/pretty.yml/badge.svg)[Issue](https://img.shields.io/github/issues/pscedu/singularity-' + repository + ')![forks](https://img.shields.io/github/forks/pscedu/singularity-' + repository + ')![Stars](https://img.shields.io/github/stars/pscedu/singularity-' + repository + ')![License](https://img.shields.io/github/license/pscedu/singularity-' + repository + ') |\n'
+  str = '| [' + repository + '](http://github.com/pscedu/singularity-' + repository + ') | ![Status](https://github.com/pscedu/singularity-' + repository + '/actions/workflows/main.yml/badge.svg)![Status](https://github.com/pscedu/singularity-' + repository + '/actions/workflows/pretty.yml/badge.svg)![Issue](https://img.shields.io/github/issues/pscedu/singularity-' + repository + ')![forks](https://img.shields.io/github/forks/pscedu/singularity-' + repository + ')![Stars](https://img.shields.io/github/stars/pscedu/singularity-' + repository + ')![License](https://img.shields.io/github/license/pscedu/singularity-' + repository + ') |\n'
   file.write( str )
 
 file.close()
